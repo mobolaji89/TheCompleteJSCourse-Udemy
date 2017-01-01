a91cf5542296f6c6ff6a95a7df77d2bc1f0b7522
Mobolaji David <davidmo@colorado.edu>
2016-12-31 22:42:44 -0800

    create variables for different data types within script.js, and make notes

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,19 @@
-console.log('Hello World')
+var name = 'John';
+console.log(name);
+
+//variable names in JS can be camelCase
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = false;
+console.log(fullAge);
+
+//Primitive JS Data Types
+  //Number: Floating point numbers, for decimals and integers.
+  //String: Sequence of characters, used for text.
+  //Boolean: Logical data type that can only be true or false.
+  //Undefined: Data type of a variable, which does not have a value yet.
+  //Null: Also means 'non-existent'.
