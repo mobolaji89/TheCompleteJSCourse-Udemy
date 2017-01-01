ad40ba26a5e9f02f68ceca3ed2b2b51516445e68
Mobolaji David <davidmo@colorado.edu>
2016-12-31 23:32:04 -0800

    comment out old variables, add new variables, and test variable mutation and type coercion

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,3 +1,5 @@
+//Lecture: variables
+/*
 var name = 'John';
 console.log(name);
 
@@ -10,6 +12,7 @@ console.log(age);
 
 var fullAge = false;
 console.log(fullAge);
+*/
 
 //Primitive JS Data Types
   //Number: Floating point numbers, for decimals and integers.
@@ -17,3 +20,33 @@ console.log(fullAge);
   //Boolean: Logical data type that can only be true or false.
   //Undefined: Data type of a variable, which does not have a value yet.
   //Null: Also means 'non-existent'.
+
+//Lecture: variables 2
+var name = 'John';
+var age = 26;
+
+console.log(name + age);
+//prints John26
+//Type Coercion - when the operands of an operator are different types, one of them will be converted to an "equivalent" value of the other operand's type.
+
+console.log(age + age);
+//prints 52
+
+var job, isMarried
+
+console.log(job)
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirty-six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+var lastName = prompt('What is the last name?')
+console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
