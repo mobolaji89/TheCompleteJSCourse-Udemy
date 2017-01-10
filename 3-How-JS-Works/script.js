8f918e1fc58842299f731090a2ccaf611b2ca721
Mobolaji David <davidmo@colorado.edu>
2017-01-09 20:55:12 -0800

    comment out hoisting code examples, and create 'this' keyword examples

--- 3-How-JS-Works/script.js
+++ 3-How-JS-Works/script.js
@@ -34,7 +34,8 @@ foo();
 //prints 23
 console.log(age);
 */
-///////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
 // Lecture: Scoping
 
 
@@ -55,11 +56,9 @@ function first() {
 }
 */
 
-
-
 // Example to show the differece between execution stack and scope chain
 
-
+/*
 var a = 'Hello!';
 first();
 
@@ -72,14 +71,55 @@ function first() {
         third()
     }
 }
+*/
+/*
 //can't access var b or c because of scope
 function third() {
     var d = 'John';
     console.log(a + d);
 }
+*/
 
-
-
-
-///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 // Lecture: The this keyword
+
+//points to the window object (global)
+//console.log(this);
+/*
+calculateAge(1985);
+
+//In a function call the this always points to the window object, because the window is the global object.
+function calculateAge(year) {
+  console.log(2016 - year);
+  console.log(this);
+}
+*/
+
+//In this scenario this refers to the john object
+var john = {
+  name: 'John',
+  yearOfBirth: 1990,
+  calculateAge: function() {
+    console.log(this);
+    console.log(2017 - this.yearOfBirth);
+    //this inside innerFunction() will point to the window object
+    /*
+    function innerFunction() {
+      console.log(this);
+    }
+    innerFunction();
+    */
+  }
+}
+
+john.calculateAge();
+
+var mike = {
+  name: 'Mike',
+  yearOfBirth: 1984
+}
+
+//method burrowing
+//this within calculateAge() will refer to the mike object
+mike.calculateAge = john.calculateAge
+mike.calculateAge();
