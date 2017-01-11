75c688a204ebb63451a7b19ee48682c409c827f2
Mobolaji David <davidmo@colorado.edu>
2017-01-10 16:57:13 -0800

    create score update conditional for dice roll, update next player using ternery operator example, and switch active player using toggle

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
@@ -54,4 +54,29 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   diceDOM.src = 'dice-' + dice + '.png'
 
   //3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1) {
+    //add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    //next player
+    //ternery operator example
+      //if activePlayer is equal to 0, then activePlayer equals 1
+      //else activePlayer = 0
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
+
+    //alternative way to add and remove
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    //when one player rolls a 1 we want to hide the dice again
+    document.querySelector('.dice').style.display = 'none';
+  }
 });
