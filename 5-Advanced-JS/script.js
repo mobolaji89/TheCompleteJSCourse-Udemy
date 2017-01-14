7d69842c8f3fcbec5194c1c58fa57af5b0b745bc
Mobolaji David <davidmo@colorado.edu>
2017-01-13 22:40:04 -0800

    comment out old code, and create primitives vs objects examples

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -46,7 +46,7 @@ console.log(mark.lastName);
 */
 
 //Object.create
-
+/*
 var personProto = {
   calculateAge: function() {
     console.log(2016 - this.yearOfBirth);
@@ -65,3 +65,51 @@ var jane = Object.create(personProto, {
   yearOfBirth: { value: 1969 },
   job: { value: 'designer' }
 });
+*/
+
+//Primitive vs objects
+
+//Variables containing primitives actually hold data inside the variable itself
+
+//Variables associated with objects do not actually contain the object, but instead contain a reference to the place in memory where the object is stored. A variable declared as an object does not have the real copy of the object; it just points to that object.
+
+//Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+//Objects
+var obj1 = {
+  name: 'John',
+  age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+//both print 30, because both point to obj1 in memory
+
+//Functions
+var age = 27;
+var obj = {
+  name: 'Jonas',
+  city: 'Lisbon'
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+//prints 27
+console.log(obj.city);
+//prints San Franscisco
+
+//variable a is a primitive that remains unchanged
+//b is a reference that points to the object, so when we change the object inside of the function, it is still reflected outside of the function
