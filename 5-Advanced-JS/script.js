fcd588771077150902f2868e0e0bb6e227ef22da
Mobolaji David <davidmo@colorado.edu>
2017-01-13 22:08:00 -0800

    comment out old code, and create Object.create examples

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -5,7 +5,7 @@
 //The Constructor's prototype property is NOT the prototype of the Constructor itself, it's the prototype of ALL instances that are created through it
 
 //When a certain method (or property) is called, the search starts in the object itself, and if it cannot be found, the search moves on to the object's prototype. This continues until the method is found: prototype chain
-
+/*
 //object literal
 var john = {
   name: 'John',
@@ -19,11 +19,9 @@ var Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
-  /*
-  this.calculateAge = function() {
-    console.log(2016 - this.yearOfBirth)
-  }
-  */
+  //this.calculateAge = function() {
+  //  console.log(2016 - this.yearOfBirth)
+  //}
 }
 
 Person.prototype.calculateAge = function() {
@@ -45,3 +43,25 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
+
+//Object.create
+
+var personProto = {
+  calculateAge: function() {
+    console.log(2016 - this.yearOfBirth);
+  }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+
+//Object.create can take a 2nd parameter
+var jane = Object.create(personProto, {
+  name: { value: 'Jane'},
+  yearOfBirth: { value: 1969 },
+  job: { value: 'designer' }
+});
