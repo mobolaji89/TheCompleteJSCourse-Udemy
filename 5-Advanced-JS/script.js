fb3d2fa533d7c024a2c5c3d107333be7ba662ca0
Mobolaji David <davidmo@colorado.edu>
2017-01-15 21:31:47 -0800

    comment out old code, and create examples of functions returning functions

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -117,7 +117,7 @@ console.log(obj.city);
 
 ////////////////////////////////////////////////////////////////////////////////
 //Lecture: Passing functions as arguments
-
+/*
 var years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
@@ -151,3 +151,31 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+*/
+
+//Lecture: Functions returning functions
+
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name + ', can you please explain what UX design is?');
+    }
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log('What subject do you teach, ' + name + '?');
+    }
+  } else {
+    return function(name) {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+
+teacherQuestion('John');
+designerQuestion('John');
+designerQuestion('Jane')
+
+interviewQuestion('teacher')('Mark');
