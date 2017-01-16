528b863f2867deccfe2b284c17956dccc35aecad
Mobolaji David <davidmo@colorado.edu>
2017-01-16 11:31:40 -0800

    comment out old code, and create closures examples

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -192,7 +192,7 @@ function game()  {
 
 game();
 */
-
+/*
 //IIFE
 //allow us to maintain data privacy
 (function ()  {
@@ -207,3 +207,43 @@ game();
   var score = Math.random() * 10;
   console.log(score >= 5 - goodLuck);
 })(5);
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+//Lecture: Closures
+
+//An inner function always has access to the variables and parameters of it's outer function, even after the outer function has returned
+
+function retirement(retirementAge) {
+  var a = ' years left until retirement.';
+  return function(yearOfBirth) {
+    var age = 2017 - yearOfBirth;
+    console.log((retirementAge - age) + a);
+  }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1989);
+retirementGermany(1989);
+retirementIceland(1989);
+
+//retirement(66)(1990);
+
+//refactor interviewQuestion as closures
+  //allows cleaner code
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === 'designer') {
+      console.log(name + ', can you please explain what UX design is?');
+    } else if (job === 'teacher') {
+      console.log('What subject do you teach, ' + name + '?');
+    } else {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+interviewQuestion('teacher')('John');
