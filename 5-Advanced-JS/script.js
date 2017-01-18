9204943dc660b74ca7a2b277f0c3f84777bb53f8
Mobolaji David <davidmo@colorado.edu>
2017-01-17 23:27:38 -0800

    comment out old code, and create bind and call examples

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -213,7 +213,7 @@ game();
 //Lecture: Closures
 
 //An inner function always has access to the variables and parameters of it's outer function, even after the outer function has returned
-
+/*
 function retirement(retirementAge) {
   var a = ' years left until retirement.';
   return function(yearOfBirth) {
@@ -247,3 +247,64 @@ function interviewQuestion(job) {
 }
 
 interviewQuestion('teacher')('John');
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+//Lecture: Bind, call and apply
+
+var john = {
+  name: 'John',
+  age: 26,
+  job: 'teacher',
+  presentation: function(style, timeOfDay) {
+    if (style === 'formal') {
+      console.log('Good ' + timeOfDay + ', Ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+    } else if (style === 'friendly') {
+      console.log('Hey! What\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+    }
+  }
+}
+
+var emily =  {
+  name: 'Emily',
+  age: 35,
+  job: 'designer'
+}
+
+john.presentation('formal', 'morning');
+
+//call
+john.presentation.call(emily, 'friendly', 'afternoon');
+
+//bind
+var johnFriendly = john.presentation.bind(john, 'friendly');
+
+johnFriendly('morning');
+johnFriendly('night');
+
+var emilyFormal = john.presentation.bind(emily, 'formal');
+emilyFormal('afternoon');
+
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+  var arrRes = [];
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el) {
+  return 2017 - el;
+}
+
+function isFullAge(limit, el) {
+  return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
