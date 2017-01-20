9a967db5d0d65d86900a7421583e3480975ab3cf
Mobolaji David <davidmo@colorado.edu>
2017-01-19 23:52:29 -0800

    comment out old code, and complete coding challenge 4

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -251,7 +251,7 @@ interviewQuestion('teacher')('John');
 
 ////////////////////////////////////////////////////////////////////////////////
 //Lecture: Bind, call and apply
-
+/*
 var john = {
   name: 'John',
   age: 26,
@@ -308,3 +308,109 @@ var ages = arrayCalc(years, calculateAge);
 var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 console.log(ages);
 console.log(fullJapan);
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+//CODING CHALLENGE 4
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an arrayCalc
+
+4. Select one random question and log it to the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct or not (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with other programmers code (Hint: we learned a special technique to do exactly that).
+
+--- Expert Level ---
+
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result).
+
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable with at this point).
+
+11. Display the score in the console. Use yet another method for this.
+*/
+
+(function() {
+  function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+  }
+
+  //displayQuestion is now apart of the prototype chain of Question
+  Question.prototype.displayQuestion = function() {
+    console.log(this.question);
+
+    for (var i = 0; i < this.answers.length; i++) {
+      console.log(i + ': ' + this.answers[i]);
+    }
+  }
+
+  Question.prototype.checkAnswer = function(ans, callback) {
+    var scr;
+    if (ans === this.correct) {
+      console.log('Correct answer!');
+      scr = callback(true);
+    } else {
+      console.log('Wrong answer. Try again!')
+      scr = callback(false);
+    }
+    this.displayScore(scr);
+  }
+
+  Question.prototype.displayScore = function(score) {
+    console.log('Your current score is: ' + score);
+    console.log('-------------------------------');
+  }
+
+  var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
+
+  var q2 = new Question('What is the name of this course\'s teacher?', ['John', 'Micheal', 'Jonas'], 2);
+
+  var q3 = new Question('What describes coding best?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+
+  var questions = [q1, q2, q3];
+
+  function score() {
+    var scr = 0;
+    return function(correct) {
+      if(correct) {
+        scr++;
+      }
+      return scr;
+    }
+  }
+
+  var keepScore = score();
+
+  function nextQuestion() {
+
+    var n = Math.floor(Math.random() * questions.length);
+
+    questions[n].displayQuestion();
+
+    var answer = prompt('Please select the correct answer.');
+
+    if (answer !== 'exit') {
+      //parseInt converts a string into an integer
+      questions[n].checkAnswer(parseInt(answer), keepScore);
+      nextQuestion();
+    }
+  }
+
+  nextQuestion();
+})();
