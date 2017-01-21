483b1088604e63c6557236f98a3a92294522f741
Mobolaji David <davidmo@colorado.edu>
2017-01-20 21:04:52 -0800

    set up event listeners for keypress events

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -1,29 +1,30 @@
-// budgetController module
+// BUDGET CONTROLLER
 var budgetController = (function() {
-  var x = 23;
 
-  var add = function(a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
 })();
 
-//UIController module
+// UI CONTROLLER
 var UIController = (function() {
 
 })();
 
-//Application controller
+// APPLICATION CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
-  return {
-    anotherPublic: function() {
-      console.log(z);
-    }
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
+    // 2. Add the item to the budget controller
+    // 3. Add the item to the UI
+    // 4. Calculate the budget
+    // 5. Display the budget
+
+    console.log('It works.');
   }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
 })(budgetController, UIController);
