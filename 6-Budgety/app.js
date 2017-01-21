874cc2bc5a42ef12c9cdaf12f5aa28a6dbf8e6c1
Mobolaji David <davidmo@colorado.edu>
2017-01-21 13:30:37 -0800

    get input for user income or expense data within UI controller, and refactor code by creating DOMstrings object

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -5,22 +5,44 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
 
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
 // APPLICATION CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
+  var DOM = UICtrl.getDOMstrings();
+
   var ctrlAddItem = function() {
     // 1. Get the field input data
+    var input = UICtrl.getInput();
+    console.log(input);
+
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget
-
-    console.log('It works.');
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 13 || event.which === 13) {
