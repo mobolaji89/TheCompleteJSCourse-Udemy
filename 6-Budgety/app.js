6a1dd9c5c2444138e2bae11458fabff58b65f08b
Mobolaji David <davidmo@colorado.edu>
2017-01-21 14:02:14 -0800

    refactor code application controller by creating init and setupEventListener functions

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -29,24 +29,35 @@ var UIController = (function() {
 
 // APPLICATION CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
-  var DOM = UICtrl.getDOMstrings();
+  var setupEventListeners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  }
 
   var ctrlAddItem = function() {
     // 1. Get the field input data
     var input = UICtrl.getInput();
-    console.log(input);
 
     // 2. Add the item to the budget controller
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget
-  }
+  };
 
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-  document.addEventListener('keypress', function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem();
+  return {
+    init: function() {
+      console.log('Application has started.');
+      setupEventListeners();
     }
-  });
+  };
+
 })(budgetController, UIController);
+
+controller.init()
