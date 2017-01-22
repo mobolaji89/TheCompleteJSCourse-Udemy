9965c4113cdbdf451ae0237d7d84de38e8f8d9bd
Mobolaji David <davidmo@colorado.edu>
2017-01-22 11:49:57 -0800

    implement addItem method within budget controller, test data structure, get added item from user in application controller, and rename add type values in index.html

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -18,11 +18,41 @@ var budgetController = (function() {
   var data = {
     allItems: {
       exp: [],
-      inc = []
-    }
+      inc: []
+    },
     totals: {
       exp: 0,
       inc: 0
+    }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on 'exp' or 'inc' type
+      if (type === 'exp') {
+          newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+          newItem = new Income(ID, des, val);
+      }
+
+      // Push new item into our data structure
+      data.allItems[type].push(newItem);
+
+      // Return the new element
+      return newItem;
+    },
+
+    testing: function() {
+      console.log(data);
     }
   };
 
@@ -70,10 +100,14 @@ var controller = (function (budgetCtrl, UICtrl) {
   }
 
   var ctrlAddItem = function() {
+    var input, newItem;
+
     // 1. Get the field input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
 
     // 2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
     // 3. Add the item to the UI
     // 4. Calculate the budget
     // 5. Display the budget
