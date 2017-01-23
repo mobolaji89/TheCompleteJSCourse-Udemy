f8f2f7121337a4f6db5a1d5d390c257f84a77fdf
Mobolaji David <davidmo@colorado.edu>
2017-01-22 18:33:36 -0800

    create clearFields method in budget controller, and implement field clearing in application controller

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -102,6 +102,22 @@ var UIController = (function() {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
+    clearFields: function() {
+      var fields, fieldsArr;
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+      //convert fields list into an array
+      fieldsArr = Array.prototype.slice.call(fields);
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = "";
+      });
+
+      //sets focus to first element of fields array
+      fieldsArr[0].focus();
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -136,8 +152,11 @@ var controller = (function (budgetCtrl, UICtrl) {
     // 3. Add the item to the UI
     UICtrl.addListItem(newItem, input.type);
 
-    // 4. Calculate the budget
-    // 5. Display the budget
+    // 4. Clear the fields
+    UICtrl.clearFields();
+
+    // 5. Calculate the budget
+    // 6. Display the budget
   };
 
   return {
