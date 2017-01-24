91857ee13430fac0a40997eb0a1eeff3504b368a
Mobolaji David <davidmo@colorado.edu>
2017-01-24 14:54:48 -0800

    define updatePercentages function in application controller

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -232,6 +232,16 @@ var controller = (function (budgetCtrl, UICtrl) {
 
   };
 
+  var updatePercentages = function() {
+
+    // 1. Calculate percentages
+
+    // 2. Read percentages from the budget controller
+
+    // 3. Update the UI with the new percentages
+
+  };
+
   var ctrlAddItem = function() {
     var input, newItem;
 
@@ -250,6 +260,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
       // 5. Calculate and update budget
       updateBudget();
+
+      // 6. Calculate and update percentages
+      updatePercentages();
     }
 
   };
@@ -273,6 +286,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
       // 3. Update and show the new budget
       updateBudget();
+
+      // 4. Calculate and update percentages
+      updatePercentages();
     }
 
   }
