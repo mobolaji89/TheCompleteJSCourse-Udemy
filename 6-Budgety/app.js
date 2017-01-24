19a103d101979452984fbfd2328986f5245f4f27
Mobolaji David <davidmo@colorado.edu>
2017-01-23 17:42:44 -0800

    create deleteItem method in budget controller

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -61,6 +61,21 @@ var budgetController = (function() {
       return newItem;
     },
 
+    deleteItem: function(type, id) {
+      var ids, index;
+
+      ids = data.allItems[type].map(function(current) {
+        return current.id;
+      });
+
+      index = ids.indexOf(id);
+
+      if (index !== -1) {
+        data.allItems[type].splice(index, 1);
+      }
+
+    },
+
     calculateBudget: function() {
       // Calculate total income and expenses__list
       calculateTotal('exp');
@@ -241,11 +256,13 @@ var controller = (function (budgetCtrl, UICtrl) {
 
       splitID = itemID.split('-');
       type = splitID[0];
-      ID = splitID[1];
+      ID = parseInt(splitID[1]);
 
       // 1. Delete the item from the data structure
+      budgetCtrl.deleteItem(type, ID);
+
       // 2. Delete the item from the UI
-      // 3. Update and show the new totals
+      // 3. Update and show the new budget
 
     }
 
