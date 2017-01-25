011b0ba4084445312bf844a793d2f5715b93c6b9
Mobolaji David <davidmo@colorado.edu>
2017-01-24 17:11:24 -0800

    create displayPercentages method in UI controller

--- 6-Budgety/app.js
+++ 6-Budgety/app.js
@@ -148,7 +148,8 @@ var UIController = (function() {
     incomeLabel: '.budget__income--value',
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
-    container: '.container'
+    container: '.container',
+    expensesPercLabel: '.item__percentage'
   }
 
   return {
@@ -220,6 +221,27 @@ var UIController = (function() {
 
     },
 
+    displayPercentages: function(percentages) {
+
+      var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
+
+      var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+          callback(list[i], i);
+        }
+      };
+
+      nodeListForEach(fields, function(current, index) {
+        if (percentages[index] > 0) {
+          current.textContent = percentages[index] + '%';
+        } else {
+          current.textContent = '---';
+        }
+
+      });
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -267,7 +289,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     var percentages = budgetCtrl.getPercentages();
 
     // 3. Update the UI with the new percentages
-    console.log(percentages);
+    UICtrl.displayPercentages(percentages);
 
   };
 
