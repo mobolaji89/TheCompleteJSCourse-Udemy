e9b8f85f241e220e0d9c9c49b78a5caeef9f9789
Mobolaji David <davidmo@colorado.edu>
2017-01-27 17:15:25 -0800

    comment out old code, and create spread operator examples

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -285,7 +285,7 @@ console.log(retirement); //38
 ////////////////////////////////////////////////////////////////////////////////
 // Lecture: Arrays
 
-const boxes = document.querySelectorAll('.box');
+//const boxes = document.querySelectorAll('.box');
 
 /*
 // ES5
@@ -294,14 +294,14 @@ boxesArr5.forEach(function(cur) {
   cur.style.backgroundColor = 'dodgerblue';
 });
 */
-
+/*
 // ES6
 const boxesArr6 = Array.from(boxes);
 //boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
 
 //alternate
 Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
-
+*/
 /*
 // ES5
 for(var i = 0; i < boxesArr5.length; i++) {
@@ -313,7 +313,7 @@ for(var i = 0; i < boxesArr5.length; i++) {
   boxesArr5[i].textContent = 'I changed to blue!'
 }
 */
-
+/*
 // ES6
 for (const cur of boxesArr6) {
   if (cur.className === 'box blue') {
@@ -322,8 +322,8 @@ for (const cur of boxesArr6) {
   }
   cur.textContent = 'I changed to blue';
 }
-
-
+*
+/*
 // ES5
 var ages = [12, 17, 8, 21, 14, 11];
 
@@ -335,7 +335,41 @@ console.log(full);
 
 console.log(full.indexOf(true)); //3
 console.log(ages[full.indexOf(true)]); //21
-
+*/
+/*
 //ES6
 console.log(ages.findIndex(cur => cur >= 18)); //3
 console.log(ages.find(cur => cur >= 18)); //21
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+//Lecture: Spread operator
+
+function addFourAges(a, b, c, d, e) {
+  return a + b + c + d;
+}
+
+var sum1 = addFourAges(18, 30, 12, 21);
+console.log(sum1); //81
+
+// ES5
+var ages = [18, 20, 12, 31];
+var sum2 = addFourAges.apply(null, ages);
+console.log(sum2); //81
+
+// ES6
+const sum3 = addFourAges(...ages);
+//... means to expand an array into it's components
+console.log(sum3); //81
+
+const familySmith = ['John', 'Jane', 'Mark'];
+const familyMiller = ['Mary', 'Bob', 'Ann'];
+const bigFamily = [...familySmith, ...familyMiller];
+console.log(bigFamily); //["John", "Jane", "Mark", "Mary", "Bob", "Ann"]
+
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
