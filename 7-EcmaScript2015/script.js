51f97c539fe6aa91f0bb6f436cd3cc36cbe078c3
Mobolaji David <davidmo@colorado.edu>
2017-01-27 16:52:03 -0800

    comment out old code, and create ES5 vs ES6 array differences examples

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -251,7 +251,7 @@ var john = ['John', 26];
 var name = john[0];
 var age = john[1];
 */
-
+/*
 // ES6
 const [name, age] = ['John', 26];
 console.log(name); //John
@@ -280,3 +280,62 @@ function calcAgeRetirement(year) {
 const [age2, retirement] = calcAgeRetirement(1990);
 console.log(age2); //27
 console.log(retirement); //38
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+// Lecture: Arrays
+
+const boxes = document.querySelectorAll('.box');
+
+/*
+// ES5
+var boxesArr5 = Array.prototype.slice.call(boxes);
+boxesArr5.forEach(function(cur) {
+  cur.style.backgroundColor = 'dodgerblue';
+});
+*/
+
+// ES6
+const boxesArr6 = Array.from(boxes);
+//boxesArr6.forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+//alternate
+Array.from(boxes).forEach(cur => cur.style.backgroundColor = 'dodgerblue');
+
+/*
+// ES5
+for(var i = 0; i < boxesArr5.length; i++) {
+  if(boxesArr5[i].className === 'box blue') {
+    //continue;
+    break;
+  }
+
+  boxesArr5[i].textContent = 'I changed to blue!'
+}
+*/
+
+// ES6
+for (const cur of boxesArr6) {
+  if (cur.className === 'box blue') {
+    //another option: if (cur.className.includes('blue'))
+    continue;
+  }
+  cur.textContent = 'I changed to blue';
+}
+
+
+// ES5
+var ages = [12, 17, 8, 21, 14, 11];
+
+var full = ages.map(function(cur) {
+  return cur >= 18;
+});
+
+console.log(full);
+
+console.log(full.indexOf(true)); //3
+console.log(ages[full.indexOf(true)]); //21
+
+//ES6
+console.log(ages.findIndex(cur => cur >= 18)); //3
+console.log(ages.find(cur => cur >= 18)); //21
